c833db1329fada67816d7f17a8b7d8789e4e0f62
Sayan Mukherjee <hello@sayanmukherjee.com>
2024-02-06 13:31:38 +0530

    updated work done till now

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,3 +1,17 @@
+import TicketList from "./TicketList";
+
 export default function page() {
-  return <main>Tickets</main>;
+  return (
+    <main>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p>
+            <small>Currently open tickets</small>
+          </p>
+        </div>
+      </nav>
+      <TicketList />
+    </main>
+  );
 }
