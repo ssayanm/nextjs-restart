34ba566a41487121b892f38aa85d38f7ecbd7a60
Sayan Mukherjee <hello@sayanmukherjee.com>
2024-02-07 10:08:37 +0530

    added a new logo, added create form link

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Logo from "./dojo-logo.png";
+import Logo from "./spi-logo.png";
 import Image from "next/image";
 
 export default function Navbar() {
@@ -8,7 +8,7 @@ export default function Navbar() {
       <Image
         src={Logo}
         alt="Logo"
-        width={70}
+        width={50}
         quality={100}
         placeholder="blur"
       />
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,18 +1,25 @@
 import { Suspense } from "react";
 import TicketList from "./TicketList";
 import Loading from "../loading";
+import Link from "next/link";
 
 export default function page() {
   return (
     <main>
-      <nav>
+      <div className="flex align-middle justify-between mx-1">
         <div>
           <h2>Tickets</h2>
           <p>
             <small>Currently open tickets</small>
           </p>
         </div>
-      </nav>
+        <div className="flex justify-center my-8">
+          <Link href="/tickets/create">
+            {" "}
+            <button className="btn-primary">Create a New Ticket</button>
+          </Link>
+        </div>
+      </div>
       <Suspense fallback={<Loading />}>
         <TicketList />
       </Suspense>
