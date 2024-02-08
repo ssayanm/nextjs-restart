1fca9e4cca1f0ee780048d98acbd314d6418b720
Sayan Mukherjee <hello@sayanmukherjee.com>
2024-02-08 13:25:28 +0530

    working with superbase for the login and signup

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,12 +1,35 @@
 "use client";
 
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
 import AuthForm from "../AuthForm";
+import { useState } from "react";
+import { Router } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Signup() {
+  const router = useRouter();
+  const { formError, setError } = useState("");
+
   const handleSubmit = async (e, email, password) => {
     e.preventDefault();
 
-    console.log("user signup", email, password);
+    const superbase = createClientComponentClient();
+    const { error } = await superbase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/api/auth/callback`,
+      },
+    });
+
+    if (error) {
+      setError(error.message);
+    }
+
+    if (!error) {
+      router.push(`/verify`);
+    }
   };
 
   return (
@@ -14,6 +37,7 @@ export default function Signup() {
       <h2 className="text-center">Sign up</h2>
 
       <AuthForm handleSubmit={handleSubmit} />
+      {formError && <div className="error">{error}</div>}
     </main>
   );
 }
