8452ec7a1c1d81f9a8dccf1c78043edd304fb036
Sayan Mukherjee <hello@sayanmukherjee.com>
2024-02-10 09:13:12 +0530

    added delete ticket

--- app/api/tickets/[id]/route.js
+++ app/api/tickets/[id]/route.js
@@ -1,19 +1,13 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
-export async function GET(_, { params }) {
+export async function DELETE(_, { params }) {
   const id = params.id;
-  const res = await fetch(`http://localhost:4000/tickets/${id}`);
 
-  const ticket = await res.json();
+  const supabase = createRouteHandlerClient({ cookies });
 
-  if (!res.ok) {
-    return NextResponse.json(
-      { error: "Cannot find the ticket" },
-      { status: 400 }
-    );
-  }
+  const { error } = await supabase.from("Tickets").delete().eq("id", id);
 
-  return NextResponse.json(ticket, {
-    status: 200,
-  });
+  return NextResponse.json({ error });
 }
