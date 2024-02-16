06210aa400d2361b3d35e8c6e9a3cc720a2a955f
Sayan Mukherjee <hello@sayanmukherjee.com>
2024-02-16 11:56:21 +0530

    changed handle delete with separate action to delete ticket

--- app/(dashboard)/tickets/[id]/DeleteButton.jsx
+++ app/(dashboard)/tickets/[id]/DeleteButton.jsx
@@ -1,42 +1,27 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useTransition } from "react";
 
 // icons & UI
 import { TiDelete } from "react-icons/ti";
+import { deleteTicket } from "../actions";
 
 export default function DeleteButton({ id }) {
-  const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
-  const handleClick = async () => {
-    setIsLoading(true);
-
-    const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
-      method: "DELETE",
-    });
-    const json = await res.json();
-
-    if (json.error) {
-      console.log(error);
-      setIsLoading(false);
-    }
-
-    if (!json.error) {
-      router.refresh();
-      router.push("/tickets");
-    }
-  };
   return (
-    <button className="btn-primary" onClick={handleClick} disabled={isLoading}>
-      {isLoading && (
+    <button
+      className="btn-primary"
+      onClick={() => startTransition(() => deleteTicket(id))}
+      disabled={isPending}
+    >
+      {isPending && (
         <>
           <TiDelete />
           Deleting....
         </>
       )}
-      {!isLoading && (
+      {!isPending && (
         <>
           <TiDelete />
           Delete Ticket
--- app/(dashboard)/tickets/actions.js
+++ app/(dashboard)/tickets/actions.js
@@ -15,10 +15,29 @@ export async function addTicket(formData) {
     data: { session },
   } = await supabase.auth.getSession();
 
+  //insert data
   const { error } = await supabase.from("Tickets").insert({
     ...ticket,
     user_email: session.user.email,
   });
+
+  if (error) {
+    throw new Error("oops, could not add the new ticket");
+  }
+
+  revalidatePath("/tickets");
+  redirect("/tickets");
+}
+
+export async function deleteTicket(id) {
+  const supabase = createServerActionClient({ cookies });
+
+  //delete data
+  const { error } = await supabase.from("Tickets").delete().eq("id", id);
+
+  if (error) {
+    throw new Error("oops, could not delete the ticket");
+  }
 
   revalidatePath("/tickets");
   redirect("/tickets");
